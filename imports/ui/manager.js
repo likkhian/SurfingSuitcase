d3c1d76c9ff3f11f7a368320957b1a7460b1f0f7
likkhian yeo <likkhian yeo>
2016-05-13 14:47:59 +0800

    intro simple edit function. populates fields with data to be edited. Note that the original entry will have to be manually deleted after.

--- imports/ui/manager.js
+++ imports/ui/manager.js
@@ -6,7 +6,6 @@ import './manager.html';
 Template.manager.events({
 	'submit .new-space'(event){
 		event.preventDefault();
-
 		//get value from form element
 		const target=event.target;
 		const spaceCat=target.category.value;
@@ -20,7 +19,6 @@ Template.manager.events({
 		const spaceWifi=target.wifi.value;
 		const spacePp=target.powerPoint.value;
 		const spaceDp=target.drinkPrice.value;
-		console.log(target)
 
 		// Insert a task into the collection
 	    // Query.insert({
@@ -53,6 +51,26 @@ Template.manager.events({
 		//Query.remove(this._id);
 		Meteor.call('query.remove',this._id);
 	},
+	'click .edit'(){
+		//make edit
+		const entry2Update=this._id
+		const entryCurser = Query.find(this._id)
+		const entry = entryCurser.fetch()
+		console.log(entry[0].text)
+		const trgt=document.getElementsByClassName("new-space")
+		console.log(trgt)
+		//fill form
+	    trgt[0].spaceName.value=entry[0].text;
+	    trgt[0].address.value=entry[0].address;
+	    trgt[0].lattitude.value=entry[0].spaceLat;
+	    trgt[0].longitude.value=entry[0].spaceLon;
+	    trgt[0].link2pic.value=entry[0].picture;
+	    trgt[0].cidLink.value=entry[0].spaceCid;
+	    trgt[0].powerPoint.value=entry[0].spacePp;
+	    trgt[0].drinkPrice.value=entry[0].spaceDp;
+	    Session.set('entry2Update',entry2Update);
+	},
+	
 })
 
 Template.manager.helpers({
