a7c1aff70d0bab54c0545c9e4d2fcc3dfec92f30
likkhian yeo <likkhian yeo>
2016-07-12 16:40:49 +0800

    Meteor update. Changed search algorithm

--- imports/api/search.js
+++ imports/api/search.js
@@ -8,98 +8,73 @@ export const SearchResults = new Mongo.Collection('searchResults');
 //search.js contains methods to match a user with his list of spaces
 
 Meteor.methods({
+	search: function (locationChoice, desiredChoice) {
+		console.log(locationChoice);
+		console.log(desiredChoice);
+		//implement lat long calculations
+		var nearestLocations = new Array();  //Longest distance is at the end of the array
+		var longestDistance = 0;
+		var maxResults = 5;
+		var dbCurser = Spaces.find();
 
-	search: function(locationChoice,desireChoice) {
-   	//implement lat long calculations
-	   	dbCurser=Spaces.find()
+		//implement option switches. Only spaces that match the requirements will be added into
+		//the distanceBetweenUs array.
 
-	   	//implement option switches. Only spaces that match the requirements will be added into
-	   	//the distanceBetweenUs array.
+		//key value pair of loc id and distances
+		dbCurser.forEach(function (entry) {
+			var spaceWifi = entry.spaceWifi;
+            if (spaceWifi === '0') {
+                entry.spaceWifi = "None";
+            } else if (spaceWifi === '1') {
+                entry.spaceWifi = "Basic";
+            } else if (spaceWifi === '2') {
+                entry.spaceWifi = "Good";
+            } else if (spaceWifi === '3') {
+                entry.spaceWifi = "Excellent";
+            } else {
+                entry.spaceWifi = "No Info";
+            }
 
-	   	switch(desireChoice){
-	   		case '1':
-			   	//key value pair of loc id and distances
-			   	var distanceBetweenUs= new Array();
-			   	dbCurser.forEach(function(entry){
-			   		console.log(parseInt(entry.spaceWifi));
-			   		//console.log(entry._id);
-			   		var idVal=entry._id;
-			   		var thisDist=distCalc(locationChoice,entry.spaceLat,entry.spaceLon)
-			   		distanceBetweenUs.push({
-			   			key: idVal,
-			   			value: Math.round(thisDist)
-			   		});
-			   	});
-			   	//console.log(distanceBetweenUs);
+			switch (desiredChoice) {
+				case '3':
+					if (parseInt(spaceWifi) <= 1 || parseInt(entry.spacePp) <= 1) {
+						break;
+					}
+				case '2':
+					if (parseInt(spaceWifi) <= 1) {
+						break;
+					}
+				case '1':
+					var thisDist = Math.round(distCalc(locationChoice, entry.spaceLat, entry.spaceLon));
+					entry.theDist = thisDist;
 
-	   		break;
-	   		case '2':
-	   			//wifi good and above only
-	   			//key value pair of loc id and distances
-			   	var distanceBetweenUs= new Array();
-			   	dbCurser.forEach(function(entry){
-			   		//console.log(entry._id);
-			   		if(parseInt(entry.spaceWifi)>1){
-				   		var idVal=entry._id;
-				   		var thisDist=distCalc(locationChoice,entry.spaceLat,entry.spaceLon)
-				   		distanceBetweenUs.push({
-				   			key: idVal,
-				   			value: Math.round(thisDist)
-				   		});
-				   	};
-			   	});
-			   	//console.log(distanceBetweenUs);
-	   		break;
-	   		case '3':
-	   			//wifi and power
-	   			//key value pair of loc id and distances
-			   	var distanceBetweenUs= new Array();
-			   	dbCurser.forEach(function(entry){
-			   		console.log(entry.spacePp);
-			   		if(parseInt(entry.spaceWifi)>1 && parseInt(entry.spacePp)>1){
-				   		var idVal=entry._id;
-				   		var thisDist=distCalc(locationChoice,entry.spaceLat,entry.spaceLon)
-				   		distanceBetweenUs.push({
-				   			key: idVal,
-				   			value: Math.round(thisDist)
-				   		});
-				   	};
-			   	});
-			   	console.log(distanceBetweenUs);
-	   		break;
-	   	}
-	   	
-	   	//sort the array
-	   	distanceBetweenUs.sort(function(a,b){
-	   		return a.value - b.value;
-	   	});
-	   	//console.log(distanceBetweenUs[0].key)
-	   	//console.log(distanceBetweenUs[0].value)
-	   	//console.log(distanceBetweenUs.length);
-	   	var sliceTo = distanceBetweenUs.length;
-	   	if (sliceTo > 5){
-	   		sliceTo = 5;
-	   	};
-	   	distanceBetweenUs = distanceBetweenUs.slice(0,sliceTo);
-	   	//console.log(distanceBetweenUs);
+					if (nearestLocations.length < maxResults && thisDist >= longestDistance) {
+						nearestLocations.push(entry);  //Push to the back
+						longestDistance = thisDist;
+					} else if (nearestLocations.length < maxResults) {
+						//Insert at the front since not the longest distance
+						nearestLocations.unshift(entry);
+					} else if (thisDist < longestDistance) {
+						nearestLocations.pop();
+						nearestLocations.push(entry);
+						longestDistance = thisDist;
+					}
+			}
+		});
 
-	   	//add points to the search results
-	   	var pts=sliceTo;//so that if only 3 locs show up, top gets 3 pts
-	   	for (i=0; i<sliceTo; i++){
-	   		var placeId=distanceBetweenUs[i].key;
-	   		var placeDist=distanceBetweenUs[i].value;
-	   		SearchResults.insert({
-	   			placeId,
-	   			placeDist,
-	   			createdAt: new Date(),
-	   		});
-	   		Spaces.update({_id:placeId},{$inc: {votes: pts, hits: 1}});
-	   		pts--;
-	   	};
-	   	//console.log(distanceBetweenUs);
-	    return distanceBetweenUs;
-	}, 
+		nearestLocations.sort(function (x, y) {
+			return x.theDist - y.theDist;
+		});
+		console.log(nearestLocations);
 
+		//add points to the search results so that if only 3 locs show up, top gets 3 pts
+		var pts = nearestLocations.length;
+		for (var i = 0; i < pts; i++) {
+			Spaces.update({ _id: nearestLocations[i]._id }, { $inc: { votes: pts - i, hits: 1 } });
+		}
+
+		return nearestLocations;
+	}
 });
 
 
--- imports/ui/results.js
+++ imports/ui/results.js
@@ -1,8 +1,9 @@
 import { Template } from 'meteor/templating';
 import { Spaces } from '../api/spaces.js';
-import './results.html';
 import { Meteor } from 'meteor/meteor';
 import { Mongo } from 'meteor/mongo';
+
+import './results.html';
 
 ToServe = new Mongo.Collection(null);
 
@@ -20,52 +21,11 @@ Template.results.helpers({
     };
     // var selectedLocation = Session.get('selectedLocation');
     // var desireChoice = Session.get('desireChoice');
-    ToServe.remove({});
-    var distanceBetweenUs = Session.get('distanceBetweenUs');
-    var resultLength = distanceBetweenUs.length;
+    var locations = Session.get('distanceBetweenUs');
+    document.getElementById('resultHeader').innerHTML = "Here are the " + locations.length + " nearest places!";
 
-    for(var i=0;i<resultLength;i++){
-      var currentAns = Spaces.findOne({_id:distanceBetweenUs[i].key});
-      var spaceCat=currentAns.spaceCat;
-      var text=currentAns.text;
-      var address=currentAns.address;
-      var spaceLat=currentAns.spaceLat;
-      var spaceLon=currentAns.spaceLon;
-      var spaceCid=currentAns.spaceCid;
-      var picture=currentAns.picture;
-      var spaceWifi=currentAns.spaceWifi;
-      if(spaceWifi==='0'){
-            spaceWifi="None";
-      }else if(spaceWifi==='1'){
-            spaceWifi="Basic";
-      }else if(spaceWifi==='2'){
-            spaceWifi="Good";
-      }else if(spaceWifi==='3'){
-            spaceWifi="Excellent";
-      }else{
-            spaceWifi="No Info";
-      };
-      var spacePp=currentAns.spacePp;
-      var spaceDp=currentAns.spaceDp;
-      theDist=distanceBetweenUs[i].value;
-      ToServe.insert({
-        theDist,
-        spaceCat,
-        text,
-        address,
-        spaceLat,
-        spaceLon,
-        spaceCid,
-        picture,
-        spaceWifi,
-        spacePp,
-        spaceDp,
-      });
-      
-    };
-    document.getElementById('resultHeader').innerHTML = "Here are the "+resultLength+" nearest places!";
-    return ToServe.find({});
-	},
+    return locations;
+  }
 });
 
 Template.SSreturn.events({
